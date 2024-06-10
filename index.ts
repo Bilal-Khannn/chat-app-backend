778ee16d4e093e6de6422e8d83ef970d91786c48
Bilal Khan <bkhan8423@gmail.com>
2024-06-10 15:38:11 +0500

    db connection

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import prisma from './utils/prisma';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+prisma
+    .$connect()
+    .then(async () => {
+        console.log('[database]: Connection to the database successful');
+    })
+    .catch((error) => {
+        console.error('[database]: Unable to connect to the database');
+        console.error(error);
+        process.exit(1); // Exit the process if unable to connect
+    });
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
